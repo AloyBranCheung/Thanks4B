0dd18e7b3126412c025cf2dacb4873cc9965972b
import-turtle <104483060+import-turtle@users.noreply.github.com>
2022-07-01 00:09:15 -0400

    initialize

---
+++ index.js
@@ -0,0 +1,20 @@
+var typed = new Typed(".auto-type", {
+  strings: [
+    "Thanks <span style='color:#ffce48'>4B.</span>",
+    "Thanks for the <span style='color:#ffce48'>love.</span>",
+    "Thanks for the <span style='color:#ffce48'>support.</span>",
+    "Thanks for <span style='color: #ffce48'> being there for me.</span>",
+    "I'm off to my next journey. Wishing you all the best :) <span style='color: #ffce48'>- Brandon</span>",
+  ],
+  typeSpeed: 70,
+  backSpeed: 40,
+  loop: false,
+});
+
+
+function reloadPage(){
+    $(".agane").click(function(){
+        location.reload();
+    })
+
+}
